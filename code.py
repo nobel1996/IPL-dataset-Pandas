abd6a81144b2692c84132376cb9bea9077a33aef
nobel1996 <nobel1996@users.noreply.github.com>
2020-05-12 01:43:53 +0530

    Code file created.

---
+++ code.py
@@ -0,0 +1,30 @@
+# --------------
+import pandas as pd 
+df = pd.read_csv(path)
+
+
+# Read the data using pandas module.
+'''df.city.unique()
+# Find the list of unique cities where matches were played
+df.isnull().sum()
+# Find the columns which contains null values if any ?
+venue_max = df.venue.value_counts()
+venue_max[:4]'''
+# List down top 5 most played venues
+df.runs
+
+# Make a runs count frequency table
+
+# How many seasons were played and in which year they were played 
+
+# No. of matches played per season
+
+# Total runs across the seasons
+
+# Teams who have scored more than 200+ runs. Show the top 10 results
+
+# What are the chances of chasing 200+ target
+
+# Which team has the highest win count in their respective seasons ?
+
+
